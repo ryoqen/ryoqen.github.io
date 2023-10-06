8d4909f1f0da278820eb25db9a506d6ab3e6e1f7
ryoqen <147133566+ryoqen@users.noreply.github.com>
2023-10-06 13:38:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-image: url('https://pibig.info/uploads/posts/2022-11/1669197599_5-pibig-info-p-akashi-seidzhuro-v-potoke-oboi-5.jpg');
+            background-size: cover;
+        }
+
+        h1 {
+            color: blue;
+            font-size: 100px;
+            text-align: center;
+            padding-top: 200px; /* Немного отступаем сверху, чтобы текст был посередине экрана */
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "<h1>Hello World</h1>";
+    ?>
+</body>
+</html>
